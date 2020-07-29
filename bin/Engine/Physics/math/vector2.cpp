bdb2f4ad19c167bdaf315d912e6dd14186ce5dfb
KiriCord <rikudosennin410@gmail.com>
2020-07-29 22:52:09 +0500

    realization override operators Vector2

--- bin/Engine/Physics/math/vector2.cpp
+++ bin/Engine/Physics/math/vector2.cpp
@@ -5,3 +5,44 @@ Vector2 Vector2::normalize()
     float normal_scale = 1.0 / (this->x * this->x + this->y * this->y);
     return Vector2(this->x / normal_scale, this->y / normal_scale);
 }
+
+float Vector2::length() {
+    return sqrt(x * x + y * y);
+}
+
+/* Vector2 by Vector2 */
+Vector2 Vector2::operator+(const Vector2& vector) const {
+    return Vector2(x + vector.x, y + vector.y);
+}
+
+Vector2 Vector2::operator-(const Vector2& vector) const {
+    return Vector2(x - vector.x, y - vector.y);
+}
+
+Vector2 Vector2::operator*(const Vector2& vector) const {
+    return Vector2(x * vector.x, y * vector.y);
+}
+
+Vector2 Vector2::operator/(const Vector2& vector) const {
+    return Vector2(x / vector.x, y / vector.y);
+}
+
+void Vector2::operator+=(const Vector2& vector) {
+    x += vector.x;
+    y += vector.y;
+}
+
+void Vector2::operator-=(const Vector2& vector) {
+    x -= vector.x;
+    y -= vector.y;
+}
+
+void Vector2::operator*=(const Vector2& vector) {
+    x *= vector.x;
+    y *= vector.y;
+}
+
+void Vector2::operator/=(const Vector2& vector) {
+    x /= vector.x;
+    y /= vector.y;
+}
--- bin/Engine/Physics/math/vector2.hpp
+++ bin/Engine/Physics/math/vector2.hpp
@@ -8,25 +8,27 @@ struct Vector2
 
     Vector2 normalize();
 
+    float length();
+
     //Vector2 by Vector2
     Vector2 operator+(const Vector2& vector) const;
     Vector2 operator-(const Vector2& vector) const;
     Vector2 operator*(const Vector2& vector) const;
     Vector2 operator/(const Vector2& vector) const;
-    Vector2& operator+=(const Vector2& vector);
-	Vector2& operator-=(const Vector2& vector);
-	Vector2& operator*=(const Vector2& vector);
-	Vector2& operator/=(const Vector2& vector);
+    void operator+=(const Vector2& vector);
+	void operator-=(const Vector2& vector);
+	void operator*=(const Vector2& vector);
+	void operator/=(const Vector2& vector);
 
     //Vector2 by float
     Vector2 operator+(float const& num) const;
     Vector2 operator-(float const& num) const;
     Vector2 operator*(float const& num) const;
     Vector2 operator/(float const& num) const;
-    Vector2& operator+=(float const& num);
-	Vector2& operator-=(float const& num);
-	Vector2& operator*=(float const& num);
-	Vector2& operator/=(float const& num);
+    void operator+=(float const& num);
+	void operator-=(float const& num);
+	void operator*=(float const& num);
+	void operator/=(float const& num);
 
     //Boolean
     bool operator==(const Vector2& vector) const;
