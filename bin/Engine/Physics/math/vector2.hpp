bdb5654eb17089b5d90d0f2b836adb177dff5905
KiriCord <rikudosennin410@gmail.com>
2020-07-29 22:05:00 +0500

    add operator vector2

--- bin/Engine/Physics/math/vector2.hpp
+++ bin/Engine/Physics/math/vector2.hpp
@@ -7,5 +7,32 @@ struct Vector2
     Vector2(float _x, float _y) :x(_x), y(_y){}
 
     Vector2 normalize();
+
+    //Vector2 by Vector2
+    Vector2 operator+(const Vector2& vector) const;
+    Vector2 operator-(const Vector2& vector) const;
+    Vector2 operator*(const Vector2& vector) const;
+    Vector2 operator/(const Vector2& vector) const;
+    Vector2& operator+=(const Vector2& vector);
+	Vector2& operator-=(const Vector2& vector);
+	Vector2& operator*=(const Vector2& vector);
+	Vector2& operator/=(const Vector2& vector);
+
+    //Vector2 by float
+    Vector2 operator+(float const& num) const;
+    Vector2 operator-(float const& num) const;
+    Vector2 operator*(float const& num) const;
+    Vector2 operator/(float const& num) const;
+    Vector2& operator+=(float const& num);
+	Vector2& operator-=(float const& num);
+	Vector2& operator*=(float const& num);
+	Vector2& operator/=(float const& num);
+
+    //Boolean
+    bool operator==(const Vector2& vector) const;
+    bool operator!=(const Vector2& vector) const;
+
+    //Print
+    friend std::ostream& operator<<( std::ostream& is,const Vector2& vector);
     
 };
